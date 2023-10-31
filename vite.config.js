25e6d3bd59f5c891f73351686bf22e9c861f070b
speed866 <3607038305@qq.com>
2023-10-31 12:38:01 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -23,8 +23,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/vue3-admin/',
-  build: {
-    outDir: 'docs'
-  }
+  base: '/vue3-admin/'
 })
